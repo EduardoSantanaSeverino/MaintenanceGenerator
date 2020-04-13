14ae83648abbb67fe5df3d402f0bd9df2cd4f080
Eduardo Santana <eduardoamparos@gmail.com>
2020-04-13 15:30:37 -0400

    Added the form interface

--- Code/MG/Generic/IfrmMainApp.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MG.Generic
-{
-    public interface IFrmMainApp
-    {
-        ICrudGenerator CrudGenerator { get; set; }
-        System.Windows.Forms.FlowLayoutPanel FlowInput { get; set; }
-        System.Windows.Forms.FlowLayoutPanel FlowOutput { get; set; }
-    }
-}
