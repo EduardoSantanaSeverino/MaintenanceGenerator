ecd016da359558c64a844b21b69ef33bec68818b
EduardoSantanaSeverino <eduardoamparos@gmail.com>
2023-07-30 13:33:52 -0400

    adding missing files

---
+++ Code/MG/SQLScripts/ItemFileToGenerate.cs
@@ -0,0 +1,33 @@
+﻿using MG.Generic;
+using System;
+using System.Collections.Generic;
+
+namespace MG.SQLScripts
+{
+    public class ItemFileToGenerate : ItemFileToGenerateBase, IItemFileToGenerate
+    {
+        public ItemFileToGenerate
+        (
+            int Id,
+            string Path,
+            string TemplateName,
+            string TemplateDirectory,
+            IClassInfoData ClassInfoData,
+            List<ItemFieldTypeTemplate> ItemFieldTypeTemplates = null,
+            FileTypes FileType = FileTypes.Others
+        )
+        : base
+        (
+            Id,
+            Enum.GetName(typeof(FileStackId), Id),
+            Path,
+            TemplateName,
+            TemplateDirectory,
+            ClassInfoData,
+            ItemFieldTypeTemplates,
+            FileType.ToString()
+        )
+        {
+        }
+    }
+}
