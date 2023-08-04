1975fd2f0a447df4833a21ce067f4b591cd429ee
EduardoSantanaSeverino <eduardoamparos@gmail.com>
2023-08-04 00:09:17 -0400

    Move class GenerateControls.cs

---
+++ Code/MG.Application/Generic/GenerateControls.cs
@@ -0,0 +1,8 @@
+namespace MG.Application.Generic;
+
+public class GenerateControls : GenerateControlsBase, IGenerateControls
+{
+    public GenerateControls(IConfiguration configuration) : base(configuration)
+    {
+    }
+}
