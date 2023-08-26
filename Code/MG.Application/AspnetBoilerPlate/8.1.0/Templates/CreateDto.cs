6fcae55ce4c8b0f47ec3372bebbf089d6291fa39
EduardoSantanaSeverino <eduardoamparos@gmail.com>
2023-08-26 12:55:44 -0400

    Fix the files so project could compile

--- Code/MG.Application/AspnetBoilerPlate/8.1.0/Templates/CreateDto.cs
+++ Code/MG.Application/AspnetBoilerPlate/8.1.0/Templates/CreateDto.cs
@@ -1,10 +1,5 @@
-using System;
-using System.ComponentModel.DataAnnotations;
 using Abp.Application.Services.Dto;
-using Abp.Auditing;
-using Abp.Authorization.Users;
 using Abp.AutoMapper;
-using Abp.Runtime.Validation;
 using XXXProjectNameXXX.Models;
 
 namespace XXXProjectNameXXX.XXXEntityPluralXXX.Dto
--- Code/MG.Application/AspnetBoilerPlate/8.1.0/Templates/Dto.cs
+++ Code/MG.Application/AspnetBoilerPlate/8.1.0/Templates/Dto.cs
@@ -1,10 +1,6 @@
 using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using Abp.Application.Services.Dto;
-using Abp.Authorization.Users;
 using Abp.AutoMapper;
-using XXXProjectNameXXX.Authorization.Users;
 using XXXProjectNameXXX.Models;
 
 namespace XXXProjectNameXXX.XXXEntityPluralXXX.Dto
